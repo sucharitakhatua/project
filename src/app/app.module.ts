cbef5eb260845f911826cf61ebf5b0bcb0011622
sucharitakhatua <sucharitak76@gmail.com>
2021-03-21 23:08:20 +0530

    hey

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ApplyComponent } from './apply/apply.component';
+
+import { FormsModule} from '@angular/forms';
+import { DataService } from './data.service';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ApplyComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [DataService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
